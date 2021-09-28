0bc2ef4d7f279cf3643a8521c1dcdc78856bc2bf
PocanPishetKod <themarcus77@yandex.ru>
2021-09-28 16:56:32 +0300

    Добавил ngbootstrap и разбил приложение на модули

---
+++ projects/yauth-client-auth/src/modules/header/header.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from "@angular/core";
+import { NavComponent } from "./components/nav/nav.component";
+
+@NgModule({
+    declarations: [
+        NavComponent
+    ],
+    exports: [
+        NavComponent
+    ],
+    providers: [
+
+    ],
+    imports: [
+
+    ]
+})
+export class HeaderModule {
+    
+}
